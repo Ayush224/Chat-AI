40dc99e8a42786ec32c7beb3b6454166db8b7b15
Ayush <yath.sharma04@gmail.com>
2025-04-28 13:41:52 +0530

    fix

--- chat-AI/vite.config.js
+++ chat-AI/vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/chat-AI/',
+  build: {
+    outDir: 'chat-AI', 
+    emptyOutDir: true,
+  },
 })
